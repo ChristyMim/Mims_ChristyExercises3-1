650f501eb8075dff414c899d1b8f41ec6b58d522
ChristyMim <christy.mims@usm.edu>
2019-10-29 18:33:26 -0500

    Initial commit

---
+++ Mims_ChristyExercises3-1/Exercises3-1.Designer.vb
@@ -0,0 +1,219 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lstResults = New System.Windows.Forms.ListBox()
+        Me.btnExercise67 = New System.Windows.Forms.Button()
+        Me.btnExercise68 = New System.Windows.Forms.Button()
+        Me.btnExercise69 = New System.Windows.Forms.Button()
+        Me.btnExercise70 = New System.Windows.Forms.Button()
+        Me.btnExercise72 = New System.Windows.Forms.Button()
+        Me.btnExercise73 = New System.Windows.Forms.Button()
+        Me.btnExercise75 = New System.Windows.Forms.Button()
+        Me.btnExercise76 = New System.Windows.Forms.Button()
+        Me.btnExercise77 = New System.Windows.Forms.Button()
+        Me.btnExercise78 = New System.Windows.Forms.Button()
+        Me.btnExercise79 = New System.Windows.Forms.Button()
+        Me.btnExercise80 = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(235, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(219, 37)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Exercises 3-1"
+        '
+        'lstResults
+        '
+        Me.lstResults.FormattingEnabled = True
+        Me.lstResults.Location = New System.Drawing.Point(214, 105)
+        Me.lstResults.Name = "lstResults"
+        Me.lstResults.Size = New System.Drawing.Size(267, 69)
+        Me.lstResults.TabIndex = 1
+        '
+        'btnExercise67
+        '
+        Me.btnExercise67.Location = New System.Drawing.Point(37, 230)
+        Me.btnExercise67.Name = "btnExercise67"
+        Me.btnExercise67.Size = New System.Drawing.Size(94, 42)
+        Me.btnExercise67.TabIndex = 2
+        Me.btnExercise67.Text = "Exercise 67"
+        Me.btnExercise67.UseVisualStyleBackColor = True
+        '
+        'btnExercise68
+        '
+        Me.btnExercise68.Location = New System.Drawing.Point(142, 230)
+        Me.btnExercise68.Name = "btnExercise68"
+        Me.btnExercise68.Size = New System.Drawing.Size(94, 42)
+        Me.btnExercise68.TabIndex = 3
+        Me.btnExercise68.Text = "Exercise 68"
+        Me.btnExercise68.UseVisualStyleBackColor = True
+        '
+        'btnExercise69
+        '
+        Me.btnExercise69.Location = New System.Drawing.Point(247, 230)
+        Me.btnExercise69.Name = "btnExercise69"
+        Me.btnExercise69.Size = New System.Drawing.Size(94, 42)
+        Me.btnExercise69.TabIndex = 4
+        Me.btnExercise69.Text = "Exercise 69"
+        Me.btnExercise69.UseVisualStyleBackColor = True
+        '
+        'btnExercise70
+        '
+        Me.btnExercise70.Location = New System.Drawing.Point(352, 230)
+        Me.btnExercise70.Name = "btnExercise70"
+        Me.btnExercise70.Size = New System.Drawing.Size(94, 42)
+        Me.btnExercise70.TabIndex = 5
+        Me.btnExercise70.Text = "Exercise 70"
+        Me.btnExercise70.UseVisualStyleBackColor = True
+        '
+        'btnExercise72
+        '
+        Me.btnExercise72.Location = New System.Drawing.Point(457, 230)
+        Me.btnExercise72.Name = "btnExercise72"
+        Me.btnExercise72.Size = New System.Drawing.Size(94, 42)
+        Me.btnExercise72.TabIndex = 6
+        Me.btnExercise72.Text = "Exercise 72"
+        Me.btnExercise72.UseVisualStyleBackColor = True
+        '
+        'btnExercise73
+        '
+        Me.btnExercise73.Location = New System.Drawing.Point(562, 230)
+        Me.btnExercise73.Name = "btnExercise73"
+        Me.btnExercise73.Size = New System.Drawing.Size(94, 42)
+        Me.btnExercise73.TabIndex = 7
+        Me.btnExercise73.Text = "Exercise 73"
+        Me.btnExercise73.UseVisualStyleBackColor = True
+        '
+        'btnExercise75
+        '
+        Me.btnExercise75.Location = New System.Drawing.Point(37, 302)
+        Me.btnExercise75.Name = "btnExercise75"
+        Me.btnExercise75.Size = New System.Drawing.Size(94, 42)
+        Me.btnExercise75.TabIndex = 8
+        Me.btnExercise75.Text = "Exercise 75"
+        Me.btnExercise75.UseVisualStyleBackColor = True
+        '
+        'btnExercise76
+        '
+        Me.btnExercise76.Location = New System.Drawing.Point(142, 302)
+        Me.btnExercise76.Name = "btnExercise76"
+        Me.btnExercise76.Size = New System.Drawing.Size(94, 42)
+        Me.btnExercise76.TabIndex = 9
+        Me.btnExercise76.Text = "Exercise 76"
+        Me.btnExercise76.UseVisualStyleBackColor = True
+        '
+        'btnExercise77
+        '
+        Me.btnExercise77.Location = New System.Drawing.Point(247, 302)
+        Me.btnExercise77.Name = "btnExercise77"
+        Me.btnExercise77.Size = New System.Drawing.Size(94, 42)
+        Me.btnExercise77.TabIndex = 10
+        Me.btnExercise77.Text = "Exercise 77"
+        Me.btnExercise77.UseVisualStyleBackColor = True
+        '
+        'btnExercise78
+        '
+        Me.btnExercise78.Location = New System.Drawing.Point(352, 302)
+        Me.btnExercise78.Name = "btnExercise78"
+        Me.btnExercise78.Size = New System.Drawing.Size(94, 42)
+        Me.btnExercise78.TabIndex = 11
+        Me.btnExercise78.Text = "Exercise 78"
+        Me.btnExercise78.UseVisualStyleBackColor = True
+        '
+        'btnExercise79
+        '
+        Me.btnExercise79.Location = New System.Drawing.Point(457, 302)
+        Me.btnExercise79.Name = "btnExercise79"
+        Me.btnExercise79.Size = New System.Drawing.Size(94, 42)
+        Me.btnExercise79.TabIndex = 12
+        Me.btnExercise79.Text = "Exercise 79"
+        Me.btnExercise79.UseVisualStyleBackColor = True
+        '
+        'btnExercise80
+        '
+        Me.btnExercise80.Location = New System.Drawing.Point(562, 302)
+        Me.btnExercise80.Name = "btnExercise80"
+        Me.btnExercise80.Size = New System.Drawing.Size(94, 42)
+        Me.btnExercise80.TabIndex = 13
+        Me.btnExercise80.Text = "Exercise 80"
+        Me.btnExercise80.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(303, 368)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(94, 42)
+        Me.btnExit.TabIndex = 14
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(697, 465)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnExercise80)
+        Me.Controls.Add(Me.btnExercise79)
+        Me.Controls.Add(Me.btnExercise78)
+        Me.Controls.Add(Me.btnExercise77)
+        Me.Controls.Add(Me.btnExercise76)
+        Me.Controls.Add(Me.btnExercise75)
+        Me.Controls.Add(Me.btnExercise73)
+        Me.Controls.Add(Me.btnExercise72)
+        Me.Controls.Add(Me.btnExercise70)
+        Me.Controls.Add(Me.btnExercise69)
+        Me.Controls.Add(Me.btnExercise68)
+        Me.Controls.Add(Me.btnExercise67)
+        Me.Controls.Add(Me.lstResults)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lstResults As ListBox
+    Friend WithEvents btnExercise67 As Button
+    Friend WithEvents btnExercise68 As Button
+    Friend WithEvents btnExercise69 As Button
+    Friend WithEvents btnExercise70 As Button
+    Friend WithEvents btnExercise72 As Button
+    Friend WithEvents btnExercise73 As Button
+    Friend WithEvents btnExercise75 As Button
+    Friend WithEvents btnExercise76 As Button
+    Friend WithEvents btnExercise77 As Button
+    Friend WithEvents btnExercise78 As Button
+    Friend WithEvents btnExercise79 As Button
+    Friend WithEvents btnExercise80 As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Mims_ChristyExercises3-1/Exercises3-1.vb
@@ -0,0 +1,148 @@
+﻿'Program Name: Exercises 3-1
+'Programmer: Christy Mims
+'Date: 1/20/17
+'Description: This program allows the user to see the results of the problems in Exercise 
+'3-1: 67, 68, 69, 70, 72, 73, 75, 76, 77, 78, 79, 80.  The program displays the results 
+'through the use of buttons and a list box.
+Public Class Form1
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+        'This button allows the user to close the program.
+    End Sub
+
+    Private Sub btnExercise67_Click(sender As Object, e As EventArgs) Handles btnExercise67.Click
+        Dim revenue As Decimal, costs As Decimal, Profit As Decimal
+        revenue = 98456
+        costs = 45000
+        Profit = revenue - costs
+        lstResults.Items.Clear()
+        lstResults.Items.Add(Profit)
+        'This procedure displays the results of profits made and performs the calculations
+        'for the user.
+    End Sub
+
+    Private Sub btnExercise68_Click(sender As Object, e As EventArgs) Handles btnExercise68.Click
+        Dim costPerShare As Decimal, numberOfShares As Decimal, amount As Decimal
+        costPerShare = 25.625D
+        numberOfShares = 400
+        amount = costPerShare * numberOfShares
+        lstResults.Items.Clear()
+        lstResults.Items.Add(amount)
+        'This procedure calculates the data for the user and displays the answer in a list
+        'box.
+    End Sub
+
+    Private Sub btnExercise69_Click(sender As Object, e As EventArgs) Handles btnExercise69.Click
+        Dim price As Decimal, discountPercent As Decimal, markdown As Decimal
+        price = 19.95D
+        discountPercent = 30
+        markdown = (discountPercent / 100) * price
+        price = price - markdown
+        lstResults.Items.Clear()
+        lstResults.Items.Add(Math.Round(price, 2))
+        'This procedure calculates a markdown price for the user and displays the answer
+        'in a list box.
+    End Sub
+
+    Private Sub btnExercise70_Click(sender As Object, e As EventArgs) Handles btnExercise70.Click
+        Dim fixedCosts As Decimal, pricePerUnit As Decimal, costPerUnit As Decimal, breakEvenPoint As Decimal
+        fixedCosts = 5000
+        pricePerUnit = 8
+        costPerUnit = 6
+        breakEvenPoint = fixedCosts / (pricePerUnit - costPerUnit)
+        lstResults.Items.Clear()
+        lstResults.Items.Add(breakEvenPoint)
+        'This procedure calculates and displays the results of the data in a list box for 
+        'the user.
+    End Sub
+
+    Private Sub btnExercise72_Click(sender As Object, e As EventArgs) Handles btnExercise72.Click
+        Dim balance As Decimal
+        balance = 100
+        balance = balance * 0.05D + 100
+        balance = balance * 0.05D + 100
+        balance = balance * 0.05D
+        lstResults.Items.Clear()
+        lstResults.Items.Add(Math.Round(balance, 2))
+        'Through this procedure the data included in the code is calculated and displayed
+        'in a list box for the user.
+    End Sub
+
+    Private Sub btnExercise73_Click(sender As Object, e As EventArgs) Handles btnExercise73.Click
+        Dim purchasePrice As Decimal, sellingPrice As Decimal, markup As Decimal, percentageMarkup As Decimal
+        purchasePrice = 215.5D
+        sellingPrice = 644.99D
+        markup = sellingPrice - purchasePrice
+        percentageMarkup = 100 * (markup / purchasePrice)
+        lstResults.Items.Clear()
+        lstResults.Items.Add(percentageMarkup)
+        'The data provided is calculated and then displayed in a list box for the user.
+    End Sub
+
+    Private Sub btnExercise75_Click(sender As Object, e As EventArgs) Handles btnExercise75.Click
+        Dim corn As Decimal, farm As Decimal, tonsOfCorn As Decimal
+        corn = 18
+        farm = 30
+        tonsOfCorn = corn * farm
+        lstResults.Items.Clear()
+        lstResults.Items.Add(tonsOfCorn)
+        'This procedure allows data to be computed and then displayed in a display box for
+        'the user.
+    End Sub
+
+    Private Sub btnExercise76_Click(sender As Object, e As EventArgs) Handles btnExercise76.Click
+        Dim velocity As Decimal, height As Decimal, time As Decimal
+        velocity = 50
+        height = 5
+        time = 3
+        height = (-16 * time ^ 2) + (velocity * time) + height
+        lstResults.Items.Clear()
+        lstResults.Items.Add(height)
+        'This procedure calculates and displays the data in a list box for the user.
+    End Sub
+
+    Private Sub btnExercise77_Click(sender As Object, e As EventArgs) Handles btnExercise77.Click
+        Dim time As Decimal, speed As Decimal, distance As Decimal
+        time = 5
+        distance = 233
+        speed = distance / time
+        lstResults.Items.Clear()
+        lstResults.Items.Add(speed)
+        'This procedure allows the user to see the data after it has been calculated.
+    End Sub
+
+    Private Sub btnExercise78_Click(sender As Object, e As EventArgs) Handles btnExercise78.Click
+        Dim odometerStart As Decimal, odometerEnd As Decimal, gallons As Decimal, milesPerGallon As Decimal
+        odometerStart = 23352
+        odometerEnd = 23695
+        gallons = 14
+        milesPerGallon = (odometerEnd - odometerStart) / 14
+        lstResults.Items.Clear()
+        lstResults.Items.Add(milesPerGallon)
+        'This procedure calculates the data for the user then displays in a list box.
+    End Sub
+
+    Private Sub btnExercise79_Click(sender As Object, e As EventArgs) Handles btnExercise79.Click
+        Dim gallonsOfWater As Decimal, population As Decimal, year As Decimal, endOfYear As Decimal
+        gallonsOfWater = 1600
+        population = 315000000
+        year = 365
+        endOfYear = population * gallonsOfWater * year
+        lstResults.Items.Clear()
+        lstResults.Items.Add(endOfYear)
+        'This procedure allows the user to see data displayed in a list box after the data
+        'has been calculated.
+    End Sub
+
+    Private Sub btnExercise80_Click(sender As Object, e As EventArgs) Handles btnExercise80.Click
+        Dim pizzaPerSecond As Decimal, day As Decimal, minute As Decimal, hour As Decimal, slicesPerDay As Decimal
+        pizzaPerSecond = 350
+        minute = 60
+        hour = 60
+        day = 24
+        slicesPerDay = pizzaPerSecond * minute * hour * day
+        lstResults.Items.Clear()
+        lstResults.Items.Add(slicesPerDay)
+        'This procedure calculates and displays the data in a list box for the user.
+    End Sub
+End Class
